f6362b0eacbc81cca4f50fca45f17178f987f078
hemarydem <hamedremy@hotmail.fr>
2023-04-27 00:15:30 +0200

    wip authentification feature create auth interceptor added package moment

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-
+import * as moment from "moment";
 @Injectable({
   providedIn: 'root'
 })
@@ -10,8 +10,33 @@ export class AuthService {
   }
 
   login(email:string, password:string ) {
-    return this.http.post<any>('/api/login', {email, password});//TODO write good url
-      // this is just the HTTP call,                           //TODO change type from any to User
-      // we still need to handle the reception of the token
+    return this.http.post<any>('https://api.game7app.com/auth', {email, password});//TODO check if its write good url
+                                                                                            // TODO change type from any to User
+  }
+
+  private setSession(authResult :any) {     // TODO change type from any
+    const expiresAt = moment().add(authResult.expiresIn,'second');
+
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem("expires_at", JSON.stringify(expiresAt.valueOf()) );
+  }
+
+  logout() {
+    localStorage.removeItem("id_token");
+    localStorage.removeItem("expires_at");
+  }
+
+  public isLoggedIn() {
+    return moment().isBefore(this.getExpiration());
+  }
+
+  isLoggedOut() {
+    return !this.isLoggedIn();
+  }
+
+  getExpiration() {
+    const expiration = localStorage.getItem("expires_at");
+    const expiresAt = JSON.parse(expiration as string);
+    return moment(expiresAt);
   }
 }
