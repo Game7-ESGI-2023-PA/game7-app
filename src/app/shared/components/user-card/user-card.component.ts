fec5494a47995f7245402ea7ceb49f8dceacb407
Marwan <marwan.boubchir@outlook.fr>
2023-04-28 18:09:08 +0200

    feat: Profil Card

---
+++ src/app/shared/components/user-card/user-card.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-user-card',
+  templateUrl: './user-card.component.html',
+  styleUrls: ['./user-card.component.css']
+})
+export class UserCardComponent implements OnInit {
+
+  @Input() username: string | undefined;
+  @Input() photoUrl: string | undefined;
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HttpClientModule } from '@angular/common/http';
+import { UserCardComponent } from './components/user-card/user-card.component';
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
     NavbarComponent,
+    UserCardComponent,
+  ],
+  exports: [
+    UserCardComponent
   ],
   imports: [
     CommonModule,
     HttpClientModule,
+    MatCardModule,
+    MatButtonModule,
+    NgOptimizedImage,
   ]
 })
 export class SharedModule { }
