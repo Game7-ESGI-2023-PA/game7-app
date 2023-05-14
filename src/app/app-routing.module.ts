c541eaf6869cc6a3e0c2641de9590bd3518eadb8
saziz <shan.azizh@gmail.com>
2023-05-14 17:57:52 +0200

    feat: add guard on landing page and no result message on search

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,6 +26,7 @@ const routes: Routes = [
         path: '',
         data: { title: 'Game7' },
         component: LandingComponent,
+        canActivate: [alreadyAuthenticatedGuard]
       },
       {
         path: 'user-search',
