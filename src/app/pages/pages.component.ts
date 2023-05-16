d3b0859c242509c57168d96cdc4815b49a479d12
saziz <shan.azizh@gmail.com>
2023-05-16 13:02:06 +0200

    feat: show navbar only when authenticated

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserService } from '../shared/services/user.service';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-pages',
@@ -7,4 +9,7 @@ import { Component } from '@angular/core';
 })
 export class PagesComponent {
 
+  showNavbar() {
+    return AuthService.isAuthenticated();
+  }
 }
