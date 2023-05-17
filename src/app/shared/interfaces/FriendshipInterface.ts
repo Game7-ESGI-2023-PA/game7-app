a3595cc7911eb4fc2a44d6abf93afa1988d007f2
saziz <shan.azizh@gmail.com>
2023-05-17 21:43:53 +0200

    feat: friendship service

---
+++ src/app/shared/interfaces/FriendshipInterface.ts
@@ -0,0 +1,18 @@
+import { UserInterface } from "./UserInterface";
+
+export interface FriendRequestInterface {
+  id: string;
+  from: UserInterface;
+  to: UserInterface;
+  status: Status;
+}
+
+export interface FriendsInterface {
+  friends: UserInterface[];
+}
+
+export enum Status {
+  Pending = "pending",
+  Accepted = "accepted",
+  Refused = "refused"
+}
---
+++ src/app/shared/services/friends.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { FriendRequestInterface, FriendsInterface } from "../interfaces/FriendshipInterface";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FriendsService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  mySentFriendRequest() {
+    return this.http.get<FriendRequestInterface[]>('/me/sent/friend_requests');
+  }
+
+  myReceivedFriendRequest() {
+    return this.http.get<FriendRequestInterface[]>('/me/received/friend_requests');
+  }
+
+  myFriends() {
+    return this.http.get<FriendsInterface>('/me/friendship');
+  }
+
+  askForFriend(to: string) {
+    return this.http.post<FriendRequestInterface>('/friend_requests', {
+      to: `/api/users/${to}`
+    });
+  }
+
+  answerRequest(requestId: string, status: string){
+    return this.http.put<FriendRequestInterface>(`/friend_requests/${requestId}/answer/`, {
+      status: status
+    });
+  }
+}
