562806a4a0a12dbbfa50b12e86f4ca988ff40982
saziz <shan.azizh@gmail.com>
2023-07-25 12:34:42 +0200

    feat: user in navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,16 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { UserService } from '../../services/user.service';
+import { UserInterface } from '../../interfaces/UserInterface';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   @Input() photoUrl: string | undefined;
+  currentUser: UserInterface | undefined;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.me().subscribe({
+      next: (res) => {
+        this.currentUser = res;
+      }
+    })
+  }
 
   redirectToHome() {
     this.router.navigate(['/user-profile']);
